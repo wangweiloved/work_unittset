9011dbc7685968e9ded9c8692131b05960279910
wangweiloved <13007188787@163.com>
2019-05-12 18:39:09 +0800

    def调用

---
+++ work1_unittest/work_def.py
@@ -0,0 +1,17 @@
+# import requests
+#
+# session=requests.session()
+# resp=session.request("post",url="http://47.107.168.87:8080/futureloan/mvc/api/member/login",data=None)
+# print(resp.text)
+class wang():
+
+    def w(self,a):
+        self.a = a
+        print(a)
+
+    def we(self):
+        print(self.a)
+
+www = wang()
+www.w(2)
+www.we()
